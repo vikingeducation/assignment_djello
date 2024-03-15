e3c6e80f251e25dc02f1253316d75b6cdcee0aab
Yi-Xuan Lau <yixuan.lau@gmail.com>
2024-03-15 09:09:58 +0800

    Specify ruby version

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['CORS_ORIGINS']
+    origins '*'
+    # origins ENV['CORS_ORIGINS']
     resource '*',
       headers: :any,
       :methods => [:get, :post, :put, :delete]
